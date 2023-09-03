e68be72d9f2c84585e24eeaaae837ac2bf30cf44
yehezkieldio <yehezkieldio35@gmail.com>
2023-09-04 04:47:07 +0800

    feat: add stuff to the hotel module

--- src/hotel/hotel.module.ts
+++ src/hotel/hotel.module.ts
@@ -3,19 +3,27 @@ import { RoomFactory } from "./domain/factories/room.factory";
 import { InjectionToken } from "./application/injection-token";
 import { RoomRepositoryImplement } from "./infrastructure/repositories/room-repository.implement";
 import { CqrsModule } from "@nestjs/cqrs";
+import { RoomController } from "./interfaces/room.controller";
+import { RoomQueryImplement } from "./infrastructure/queries/room-query.implement";
+import { FindRoomHandler } from "./application/handlers/query-handlers/find-room.handler";
 
 const infrastructure: Provider[] = [
     {
         provide: InjectionToken.ROOM_REPOSITORY,
         useClass: RoomRepositoryImplement,
     },
+    {
+        provide: InjectionToken.ROOM_QUERY,
+        useClass: RoomQueryImplement,
+    },
 ];
-const application = [];
+const application = [FindRoomHandler];
 const domain = [RoomFactory];
+const controllers = [RoomController];
 
 @Module({
     imports: [CqrsModule],
-    controllers: [],
+    controllers: [...controllers],
     providers: [...infrastructure, ...application, ...domain],
 })
 export class HotelModule {}
